c7cdb954349c1e6d460269e47920ef42a7168824
Bob_Dev <akhozwork@gmail.com>
2025-08-12 15:31:13 +0200

    menu + fixes

---
+++ js/menu-open-anim.js
@@ -0,0 +1,7 @@
+/* Simple appearence with animation AN-1*/
+function openNav() {
+    document.getElementById("mySidenav").style.width = "450px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
